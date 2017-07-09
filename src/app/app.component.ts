9a6f137871b2491cb7cc2b6c0621bb8377fd82a5
wgf4242 <wgf4242@gmail.com>
2017-07-09 21:12:48 +0800

    7. user-input.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Hero } from "app/hero";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+/*  template: `
+    <h1>title</h1>
+    <h2>My favourite hero is {{myhero.name}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{hero.name}}
+      </li>
+    </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+  `,*/
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  heroes = [
+    new Hero(1, 'windstorm'),
+    new Hero(13, 'Bambasto'),
+    new Hero(15, 'Mangeta'),
+    new Hero(20, 'Tornado')
+  ]
+  myhero = this.heroes[0];
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ClickMeComponent } from "app/click-me.component";
+import { LittleTourComponent } from "app/little-four.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickMeComponent,
+    LittleTourComponent
   ],
   imports: [
     BrowserModule
